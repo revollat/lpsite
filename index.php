2b176a9fc26d10fa9f302dc313095779113df186
o_revollat <revollat@gmail.com>
2017-02-17 10:48:03 +0000

    debut

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 require_once "init.php";
 
 $app->get('/', function () use ($app)  {
-        return $app['twig']->render('accueil.html.twig');
+        return $app['twig']->render('pages/accueil.html.twig');
 });
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return $app['twig']->render('hello.html.twig', array(
+    return $app['twig']->render('pages/hello.html.twig', array(
         'name' => $name,
     ));
-});
+})->value('name', 'World');
 
 $app->run();
